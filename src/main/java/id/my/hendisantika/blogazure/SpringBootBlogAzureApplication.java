47ee4ccb68454db8791c8d126140fde2a34c558b
hendisantika <hendisantika@yahoo.co.id>
2025-01-20 12:02:10 +0700

    Add Init Data in Main Class🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/blogazure/SpringBootBlogAzureApplication.java
+++ src/main/java/id/my/hendisantika/blogazure/SpringBootBlogAzureApplication.java
@@ -1,7 +1,16 @@
 package id.my.hendisantika.blogazure;
 
+import id.my.hendisantika.blogazure.model.Author;
+import id.my.hendisantika.blogazure.model.Comment;
+import id.my.hendisantika.blogazure.model.Post;
+import id.my.hendisantika.blogazure.repository.AuthorRepository;
+import id.my.hendisantika.blogazure.repository.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 @SpringBootApplication
 public class SpringBootBlogAzureApplication {
@@ -10,4 +19,16 @@ public class SpringBootBlogAzureApplication {
         SpringApplication.run(SpringBootBlogAzureApplication.class, args);
     }
 
+    @Profile({"dev","prod"})
+    @Bean
+    CommandLineRunner run(PostRepository postRepository, AuthorRepository authorRepository) {
+        return args -> {
+            AggregateReference<Author,Integer> author = AggregateReference.to(authorRepository.save(new Author(null, "Dan", "Vega", "danvega@gmail.com", "dvega")).id());
+
+            Post post = new Post( "Yuji's First Post", "This is Yuji's First Post",author);
+            post.addComment(new Comment( "Itadori Yuji", "This is a comment"));
+            post.addComment(new Comment( "Ryoumen Sukuna", "This is another comment"));
+            postRepository.save(post);
+        };
+    }
 }
