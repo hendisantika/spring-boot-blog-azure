29d7ce07d419adf2890d897624e3d3e88727ebe1
hendisantika <hendisantika@yahoo.co.id>
2025-01-20 12:11:40 +0700

    Add TestDataLoader🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/blogazure/TestDataLoader.java
+++ src/test/java/id/my/hendisantika/blogazure/TestDataLoader.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.blogazure;
 
+import id.my.hendisantika.blogazure.model.Author;
+import id.my.hendisantika.blogazure.model.Comment;
+import id.my.hendisantika.blogazure.model.Post;
 import id.my.hendisantika.blogazure.repository.AuthorRepository;
 import id.my.hendisantika.blogazure.repository.PostRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -26,5 +30,14 @@ public class TestDataLoader implements CommandLineRunner {
     public TestDataLoader(AuthorRepository authors, PostRepository posts) {
         this.authors = authors;
         this.posts = posts;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        AggregateReference<Author,Integer> author = AggregateReference.to(authors.save(new Author(null, "Dan", "Vega", "danvega@gmail.com", "dvega")).id());
+        Post post = new Post("Hello, World!","Welcome to my Spring Blog", author);
+        post.addComment(new Comment( "Itadori Yuji", "This is a comment"));
+        post.addComment(new Comment( "Ryoumen Sukuna", "This is another comment"));
+        posts.save(post);
     }
 }
