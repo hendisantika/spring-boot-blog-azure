4ce0807227b7359003bfe1db50f2aa3bfd737fc0
hendisantika <hendisantika@yahoo.co.id>
2025-01-20 12:03:16 +0700

    Add findById in PostController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/blogazure/controller/PostController.java
+++ src/main/java/id/my/hendisantika/blogazure/controller/PostController.java
@@ -6,6 +6,7 @@ import id.my.hendisantika.blogazure.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +32,10 @@ public class PostController {
     @GetMapping
     public Iterable<Post> findAll() {
         return posts.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post findById(@PathVariable("id") Post post) {
+        return post;
     }
 }
